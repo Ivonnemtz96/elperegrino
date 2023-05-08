05dbfcd0de3f149dcec0bf7b403f2a8d621d8676
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-08 15:09:53 -0700

    nuevos archivos al servidor

---
+++ includes/head.php
@@ -0,0 +1,18 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Restho - Resturent HTML Template</title>
+    <link rel="icon" href="assets/images/icon/logo-icon.svg" type="image/gif" sizes="20x20" />
+    <link rel="stylesheet" href="assets/css/all.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/boxicons.min.css" />
+    <link rel="stylesheet" href="assets/css/bootstrap-icons.css" />
+    <link rel="stylesheet" href="assets/css/jquery-ui.css" />
+    <link rel="stylesheet" href="assets/css/swiper-bundle.css" />
+    <link rel="stylesheet" href="assets/css/nice-select.css" />
+    <link rel="stylesheet" href="assets/css/magnific-popup.css" />
+    <link rel="stylesheet" href="assets/css/jquery.fancybox.min.css" />
+    <link rel="stylesheet" href="assets/css/odometer.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
