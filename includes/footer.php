ce91fee31adeeb69b90386fd87318a41384b50c3
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-08 15:14:11 -0700

    archivos subidos a includes

---
+++ includes/footer.php
@@ -0,0 +1,107 @@
+<footer>
+    <div class="footer-top">
+        <div class="container">
+            <div class="row justify-content-center align-items-center gy-5">
+                <div class="col-lg-4 col-md-6 order-md-1 order-2">
+                    <div class="footer-widget one">
+                        <div class="widget-title">
+                            <h3>Our Facilities</h3>
+                        </div>
+                        <div class="menu-container">
+                            <ul>
+                                <li><a href="menu1.html">Indian Menu</a></li>
+                                <li><a href="menu1.html">Menu Item</a></li>
+                                <li><a href="reservation.html">Private Event</a></li>
+                                <li><a href="menu1.html">Italian Menu</a></li>
+                                <li><a href="category.html">Best Offer</a></li>
+                            </ul>
+                            <ul>
+                                <li><a href="category.html">Popular Item</a></li>
+                                <li><a href="menu1.html">Regular Menu</a></li>
+                                <li><a href="menu1.html">New Food</a></li>
+                                <li><a href="category.html">Special Offer</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 order-md-2 order-1">
+                    <div class="footer-widgetfooter-widget social-area">
+                        <div class="footer-logo text-center">
+                            <a href="index.html"><img src="assets/images/header1-logo.svg" alt="" /></a>
+                            <p>Established . 2022</p>
+                            <span><img src="assets/images/icon/footer-shape.svg" alt="" /></span>
+                        </div>
+                        <div class="footer-social">
+                            <ul class="social-link d-flex align-items-center justify-content-center">
+                                <li>
+                                    <a href="https://www.facebook.com/"><i class="bx bxl-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/"><i class="bx bxl-instagram-alt"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://www.pinterest.com/"><i class="bx bxl-linkedin"></i></a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/"><i class="bx bxl-twitter"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 order-3">
+                    <div class="footer-widget one">
+                        <div class="widget-title">
+                            <h3>Address Info</h3>
+                        </div>
+                        <div class="contact-info">
+                            <div class="single-contact">
+                                <span class="title">Phone:</span>
+                                <span class="content"><a href="tel:+8801776766767">+880-1776-766-767</a></span>
+                            </div>
+                            <div class="single-contact">
+                                <span class="title">Email:</span>
+                                <span class="content"><a
+                                        href="https://demo.egenslab.com/cdn-cgi/l/email-protection#91f8fff7fed1f4e9f0fce1fdf4bff2fefc"><span
+                                            class="__cf_email__"
+                                            data-cfemail="caa3a4aca58aafb2aba7baa6afe4a9a5a7">[email&#160;protected]</span></a></span>
+                            </div>
+                            <div class="single-contact">
+                                <span class="title">Fax ID:</span>
+                                <span class="content"><a href="fax:+9975667786">+99-75667-786</a></span>
+                            </div>
+                            <div class="single-contact">
+                                <span class="title">Location:</span>
+                                <span class="content"><a href="https://goo.gl/maps/2Q4gzMK8mNc1uYnL7">Mirpur
+                                        DOHS,House-167/170,<br />
+                                        Road-02 Avenue-01.</a></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-btm">
+        <div class="container">
+            <div class="row border-ttop g-2">
+                <div class="col-md-8 justify-content-md-start justify-content-center">
+                    <div class="copyright-area">
+                        <p>
+                            @Copyright by <a href="#">Egenslab</a>-2022, All Right
+                            Reserved.
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-4 d-flex justify-content-md-end justify-content-center">
+                    <div class="privacy-policy">
+                        <p>
+                            <a href="#">Privacy & Policy</a> |
+                            <a href="#">Terms and Conditions</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ includes/scripts.php
@@ -0,0 +1,16 @@
+<script data-cfasync="false"
+    src="https://demo.egenslab.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/jquery-ui.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/swiper-bundle.min.js"></script>
+<script src="assets/js/jquery.nice-select.js"></script>
+<script src="assets/js/jquery.fancybox.min.js"></script>
+<script src="assets/js/odometer.min.js"></script>
+<script src="assets/js/viewport.jquery.js"></script>
+<script src="assets/js/isotope.pkgd.min.js"></script>
+<script src="assets/js/SmoothScroll.js"></script>
+<script src="assets/js/jquery.nice-number.min.js"></script>
+<script src="assets/js/jquery.magnific-popup.min.js"></script>
+<script src="assets/js/imagesloaded.pkgd.js"></script>
+<script src="assets/js/masonry.pkgd.min.js"></script>
