ce95d331a7a057eabef57228e0ed3dc5bcaaae78
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-08 17:00:20 -0700

    se cambia title en head

--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Restho - Resturent HTML Template</title>
+    <title>El Peregrino</title>
     <link rel="icon" href="assets/images/icon/logo-icon.svg" type="image/gif" sizes="20x20" />
     <link rel="stylesheet" href="assets/css/all.css" />
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
