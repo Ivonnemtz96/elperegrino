1de865e7dd2caee2cb7cef52e65752948fa9fb38
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-05-10 10:43:06 -0700

    Se arregla el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,20 +1,11 @@
 <footer>
     <div class="footer-btm">
         <div class="container">
-            <div class="row border-ttop g-2">
-                <div class="col-md-8 justify-content-md-start justify-content-center">
+            <div class="row border-ttop g-2 justify-content-center">
+                <div class="col-md-12 text-center">
                     <div class="copyright-area">
                         <p>
-                            @Copyright by <a href="#">Egenslab</a>-2022, All Right
-                            Reserved.
-                        </p>
-                    </div>
-                </div>
-                <div class="col-md-4 d-flex justify-content-md-end justify-content-center">
-                    <div class="privacy-policy">
-                        <p>
-                            <a href="#">Privacy & Policy</a> |
-                            <a href="#">Terms and Conditions</a>
+                           El Peregrino @Copyright. Diseñado por <a target="_blank" href="https://bananagroup.mx"><strong>Banana Group Marketing</strong></a>.
                         </p>
                     </div>
                 </div>
